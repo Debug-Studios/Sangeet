140716a1ff5955adc26ef7f5f87db8ff295d71e8
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2018-07-27 11:09:14 +0530

    Added Semantic UI

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,6 +4,10 @@ import axios from 'axios';
 import App from './App';
 import router from './router';
 
+// Import Semantic UI
+import 'semantic-ui/dist/semantic.min.css';
+import 'semantic-ui/dist/semantic.min.js';
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
