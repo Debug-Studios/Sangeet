151644dea9bc6a3b971b8b503ae501341a332fec
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2018-07-27 11:37:15 +0530

    Re added yarn and added pug loader #5

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,12 +1,10 @@
+import 'bulma/css/bulma.css';
+
 import Vue from 'vue';
 import axios from 'axios';
 
 import App from './App';
 import router from './router';
-
-// Import Semantic UI
-import 'semantic-ui/dist/semantic.min.css';
-import 'semantic-ui/dist/semantic.min.js';
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
