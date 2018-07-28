ca62f4476fef5c942ccb93928422f0bc98c97df3
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2018-07-28 22:44:10 +0530

    Added empty skeleton vue components

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -7,11 +7,9 @@
 </template>
 
 <script>
-  import SystemInformation from './LandingPage/SystemInformation';
 
   export default {
     name: 'landing-page',
-    components: { SystemInformation },
     methods: {
       open(link) {
         this.$electron.shell.openExternal(link);
