fff50ada07e03f35627bb59b64574999580c0899
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2018-07-29 20:11:17 +0530

    Added Element-UI

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,15 +1,21 @@
 <template lang="pug">
   div
-    section.section
-      .container
-        h1.title
-        | Hello World
+    el-button(@click="visible=true") Button
+    el-dialog(:visible.sync="visible" title="Hello World")
+      p Try Element
 </template>
 
 <script>
 
   export default {
     name: 'landing-page',
+    /* eslint-disable */
+    data() {
+      return {
+        visible: false
+      }
+    },
+    /* eslint-enable */
     methods: {
       open(link) {
         this.$electron.shell.openExternal(link);
