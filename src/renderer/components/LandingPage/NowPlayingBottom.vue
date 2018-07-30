139fd6141d449f79747e06ced031aea67679c6ec
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2018-07-30 13:47:12 +0530

    Added initial components to Nav Bar

--- src/renderer/components/LandingPage/NowPlayingBottom.vue
+++ src/renderer/components/LandingPage/NowPlayingBottom.vue
@@ -1,18 +1,50 @@
 <template lang="pug">
   el-row.now-playing-bottom
-    el-col(:span="6")
+    el-col(:span="6" style="border-right: 1px solid white")
       el-container.center-container(style="align-items:center")
         img.image(src="http://assets.blabbermouth.net.s3.amazonaws.com/media/amityafflictionheartbreak.jpg" height="64px" width="64px")
         el-container(direction="vertical").song-container
-          h4.song-name Fight My Regrets
-          h5.song-artist Amity Affliction
+          h4.song-name {{songName}}
+          h5.song-artist {{songArtist}}
 
 
-    el-col(:span="18")
-      el-container.center-container
+    el-col(:span="16" :offset="2")
+      el-row(style="display: flex; align-items: center;")
+        el-col(:span="4")
+          h5.time-text {{playedTime}} / {{totalTime}}
+
+        el-col(:span="14")
+          el-col(:span="4").el-container.center-container
+            el-button(icon="fas fa-random" plain circle).transparent-button
+          el-col(:span="12").el-container.center-container
+            el-button(icon="fas fa-backward" plain circle).transparent-button
+            el-button(icon="fas fa-pause" plain circle type="primary")
+            el-button(icon="fas fa-forward" plain circle).transparent-button
+          el-col(:span="4").el-container.center-container
+            el-button(icon="fas fa-closed-captioning" plain circle).transparent-button
+
+        el-col(:span="6" style="align-items: center").el-container.center-container
+          el-button(icon="fas fa-volume-up" plain circle).transparent-button
+          el-col(:span="24")
+            el-slider(v-model="volume")
 
 
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      songName: 'Fight My Regrets',
+      songArtist: 'Amity Affliction',
+      totalTime: '4:40',
+      playedTime: '1:20',
+      volume: 80
+    }
+  }
+}
+</script>
+
 
 <style lang="scss" scoped>
 .center-container {
@@ -41,6 +73,18 @@
 
   .song-artist {
     font-weight: 200;
+  }
+}
+
+.transparent-button {
+  background: none;
+  border: none;
+
+  &:hover,
+  &:active,
+  &:focus {
+    background: none;
+    border: none;
   }
 }
 </style>
