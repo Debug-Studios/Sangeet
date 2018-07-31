171a220dfddb4f7577a764c3222c269a692ccd8e
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2018-07-31 10:43:55 +0530

    Added font-awesome icons to menu items.

--- src/renderer/components/LandingPage/SideMenu.vue
+++ src/renderer/components/LandingPage/SideMenu.vue
@@ -2,31 +2,31 @@
   el-menu.el-menu-vertical-demo(default-active ='1' background-color='#1b1d1c' text-color='#fff' active-text-color='#ffd04b' style='min-height:90vh;')
     router-link.plain(to='/nowPlaying')
       el-menu-item(index='1')
-        i.el-icon-menu
+        icon.item-margin.fa.fa-play
         span Now Playing
     router-link.plain(to='/songsView')
       el-menu-item(index='2')
-        i.el-icon-setting
+        icon.item-margin.fa.fa-music
         span Songs
     router-link.plain(to='/artistView')
       el-menu-item(index='3')
-        i.el-icon-document
+        icon.item-margin.fa.fa-users
         span Artist
     router-link.plain(to='/albumView')
       el-menu-item(index='4')
-        i.el-icon-menu
+        icon.item-margin.fa.fa-images
         span Albums
     router-link.plain(to='/playlistView')
       el-menu-item(index='5')
-        i.el-icon-menu
+        icon.item-margin.fa.fa-list-alt
         span Playlists
     router-link.plain(to='/settings')
       el-menu-item.plain(index='6')
-        i.el-icon-setting
+        icon.item-margin.fa.fa-sliders-h
         span Settings
     router-link.plain(to='/about')
       el-menu-item(index='7')
-        i.el-icon-menu
+        icon.item-margin.fa.fa-info-circle
         span About Us
 
 
@@ -41,6 +41,10 @@
 .plain{
   text-decoration: none;
   color: #fff;
+}
+
+.item-margin {
+  margin-right: 10px;
 }
 
 
