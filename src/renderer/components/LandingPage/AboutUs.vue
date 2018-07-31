41246d91a6c6b7e32bd83df152587d0df800ca7f
Sachin Singh Rawat <40228428+shadesrt@users.noreply.github.com>
2018-07-31 12:26:52 +0530

    updation of about us page still need work

--- src/renderer/components/LandingPage/AboutUs.vue
+++ src/renderer/components/LandingPage/AboutUs.vue
@@ -1,6 +1,41 @@
 <template lang="pug">
-  h1 HI
+  header 
+    h1 About Us
+    h3 Debug Studios
+    h5 A team of developers developing projects for fun.
+    el-row(padding-top="10px")
+      el-col(:span="10" style="box-sizing: content-box; background-color: #1b1d1c")
+        el-container.center-container(style="align-items:center")
+          img.image(src="http://assets.blabbermouth.net.s3.amazonaws.com/media/amityafflictionheartbreak.jpg" height="90px" width="90px")
+        el-container(direction="vertical" style="align-items:center; padding-bottom: 0px" )
+          h3 Prajjwal Dimri
+        el-container(direction="vertical" style="align-items:center; padding-bottom: 0px" )
+          h5 Dev
+
+      el-col(:span="10" style="box-sizing: content-box; background-color: #1b1d1c")
+        el-container.center-container(style="align-items:center")
+          img.image(src="http://assets.blabbermouth.net.s3.amazonaws.com/media/amityafflictionheartbreak.jpg" height="90px" width="90px")
+        el-container(direction="vertical" style="align-items:center")
+            h3 Ayush Bahuguna
+            h5 Dev
+
+    el-row
+      el-col(:span="10" style="box-sizing: content-box; background-color: #1b1d1c")
+        el-container.center-container(style="align-items:center")
+          img.image(src="http://assets.blabbermouth.net.s3.amazonaws.com/media/amityafflictionheartbreak.jpg" height="90px" width="90px")
+        el-container(direction="vertical" style="align-items:center")
+            h3 Sachin Singh Rawat
+            h5 UI
+
+      el-col(:span="10" style="box-sizing: content-box; background-color: #1b1d1c")
+        el-container.center-container(style="align-items:center")
+          img.image(src="http://assets.blabbermouth.net.s3.amazonaws.com/media/amityafflictionheartbreak.jpg" height="90px" width="90px")
+        el-container(direction="vertical" style="align-items:center")
+            h3 Naman Sehgal
+            h5 UI
+
 </template>
 <style scoped>
+  
 
 </style>
