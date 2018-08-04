d353df97c13b478a3d5421899196279e62c0400d
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2018-08-04 23:33:51 +0530

    Added basic songs list layout

--- src/renderer/components/LandingPage/SongsView.vue
+++ src/renderer/components/LandingPage/SongsView.vue
@@ -1,7 +1,53 @@
 <template lang="pug">
-  h1 Songs View
+  el-row
+    el-col(:span="24" v-for="index in 10" :key="0")
+      el-container
+        el-col(:span='4')
+          h5 1.
+        el-col(:span='6')
+          h5.song-name Song title
+        el-col(:span='6')
+          h5.song-artist Song Artist
+        el-col(:span='6')
+          h5.song-artist Song Album
+        el-col(:span='6')
+          el-button(icon='fa fa-play' type="primary" plain circle).transparent-button
+        
 </template>
-<style scoped>
+
+<script>
+
+import GlobalBus from './GlobalEventBus';
+export default {
+  data() {
+    return {
+      songTitle : '',
+      songArtist : '',
+      serialNumber : 1,
+    }
+  },
+
+  mounted() {
+    this.$db.find({}, (err, docs) => {
+      this.songTitle = title;
+      this.songArtist = artist;
+      serialNumber++;
+    });
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+  .song-name {
+    font-weight: 400;
+    margin-bottom: 0;
+  }
+
+  .song-artist {
+    font-weight: 200;
+  }
+
 
 </style>
 
