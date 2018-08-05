3cf86a735de23896fba003547ac3cc06e7cf9481
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2018-08-05 12:34:17 +0530

    Added songs list in SongsView and fixed height of songs view

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -5,7 +5,7 @@
       el-col(:span="6")
         side-menu
 
-      el-col(:span="17" :offset='1').el-main
+      el-col(:span="17" :offset='1' style="max-height: 90vh").el-main
         router-view
 
     el-footer
--- src/renderer/components/LandingPage/SongsView.vue
+++ src/renderer/components/LandingPage/SongsView.vue
@@ -1,17 +1,16 @@
 <template lang="pug">
   el-row
-    el-col(:span="24" v-for="index in 10" :key="0")
+    el-col(:span="24" v-for="song in songs" :key="0")
       el-container
-        el-col(:span='4')
-          h5 1.
-        el-col(:span='6')
-          h5.song-name Song title
-        el-col(:span='6')
-          h5.song-artist Song Artist
-        el-col(:span='6')
-          h5.song-artist Song Album
-        el-col(:span='6')
+        el-col(:span='2')
           el-button(icon='fa fa-play' type="primary" plain circle).transparent-button
+        el-col(:span='7')
+          h5.song-name {{song.title}}
+        el-col(:span='7')
+          h5.song-artist {{song.artist}}
+        el-col(:span='7')
+          h5.song-artist {{song.album}}
+        
         
 </template>
 
@@ -21,17 +20,15 @@ import GlobalBus from './GlobalEventBus';
 export default {
   data() {
     return {
-      songTitle : '',
-      songArtist : '',
-      serialNumber : 1,
+      songs: [],
+      serialNumber: 0
     }
   },
 
   mounted() {
     this.$db.find({}, (err, docs) => {
-      this.songTitle = title;
-      this.songArtist = artist;
-      serialNumber++;
+      console.log(docs);
+      this.songs = docs;
     });
   }
 }
@@ -41,7 +38,7 @@ export default {
 
   .song-name {
     font-weight: 400;
-    margin-bottom: 0;
+    margin-bottom: 0px;
   }
 
   .song-artist {
