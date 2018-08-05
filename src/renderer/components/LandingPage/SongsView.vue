4bbae5381e62bec06c82d6c79ebbc69ff1893d2c
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2018-08-05 13:13:30 +0530

    Added play functionality to songs in list

--- src/renderer/components/LandingPage/SongsView.vue
+++ src/renderer/components/LandingPage/SongsView.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
   el-row
-    el-col(:span="24" v-for="song in songs" :key="0")
+    el-col(:span="24" v-for="(song,index) in songs" :key="0")
       el-container
         el-col(:span='2')
-          el-button(icon='fa fa-play' type="primary" plain circle).transparent-button
+          el-button(icon='fa fa-play' type="primary" plain circle @click='playSong(index)' v-show="index != currentSongIndex").transparent-button
+          el-button(icon='el-icon-loading' type="primary" plain circle v-show="index == currentSongIndex").transparent-button
         el-col(:span='7')
           h5.song-name {{song.title}}
         el-col(:span='7')
@@ -21,7 +22,8 @@ export default {
   data() {
     return {
       songs: [],
-      serialNumber: 0
+      serialNumber: 0,
+      currentSongIndex: -1
     }
   },
 
@@ -30,6 +32,13 @@ export default {
       console.log(docs);
       this.songs = docs;
     });
+  },
+
+  methods: {
+    playSong(index) {
+      this.currentSongIndex = index;
+      GlobalBus.$emit('play-now', this.songs[index]);
+    }
   }
 }
 </script>
