46c4078a3acb96008f5b133e40bd6e58c64d1cd1
Sachin Singh Rawat <40228428+shadesrt@users.noreply.github.com>
2018-08-06 12:30:42 +0530

    added image links for contact :)

--- src/renderer/components/LandingPage/AboutUs.vue
+++ src/renderer/components/LandingPage/AboutUs.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
   header 
-    h1 About Us
-    h3 Debug Studios
-    h5 A team of developers developing projects for fun.
+    h1 Welcome
+    h3 We're Debug Studios!
+    h4 A team of developers developing projects for fun. Consisting, but not limited to :
     el-row(padding-top="10px")
       el-col(:span="10" style="box-sizing: content-box;")
         el-container.center-container(style="align-items:center")
@@ -10,17 +10,19 @@
         el-container(direction="vertical" style="align-items:center; " )
           h3.developer__name Prajjwal Dimri
           h5.developer__role Dev
-          h5.contact__link: img.image(src="http://assets.blabbermouth.net.s3.amazonaws.com/media/amityafflictionheartbreak.jpg" height="20px" width="20px") 
-
+          el-container(direction="horizontal")
+            h5.contact__link: a(href="#"): img.image(src="http://assets.blabbermouth.net.s3.amazonaws.com/media/amityafflictionheartbreak.jpg" height="20px" width="20px")
+            h5.contact__link: a(href="#"): img.image(src="http://assets.blabbermouth.net.s3.amazonaws.com/media/amityafflictionheartbreak.jpg" height="20px" width="20px")
+            
       el-col(:span="10" style="box-sizing: content-box;")
         el-container.center-container(style="align-items:center")
           img.image(src="http://assets.blabbermouth.net.s3.amazonaws.com/media/amityafflictionheartbreak.jpg" height="90px" width="90px")
         el-container(direction="vertical" style="align-items:center")
             h3.developer__name Ayush Bahuguna
             h5.developer__role Dev
-            h5.contact__link: a(class="tel" href="#") Telegram 
-              a(class="git" href="#") GitHub
-
+            el-container(direction="horizontal")
+              h5.contact__link: a(href="#"): img.image(src="http://assets.blabbermouth.net.s3.amazonaws.com/media/amityafflictionheartbreak.jpg" height="20px" width="20px")
+              h5.contact__link: a(href="#"): img.image(src="http://assets.blabbermouth.net.s3.amazonaws.com/media/amityafflictionheartbreak.jpg" height="20px" width="20px")
     el-row
       el-col(:span="10" style="box-sizing: content-box;")
         el-container.center-container(style="align-items:center")
@@ -28,8 +30,9 @@
         el-container(direction="vertical" style="align-items:center")
             h3.developer__name Sachin Singh Rawat
             h5.developer__role UI
-            h5.contact__link: a(class="tel" href="#") Telegram 
-              a(class="git" href="#") GitHub
+            el-container(direction="horizontal")
+              h5.contact__link: a(href="#"): img.image(src="http://assets.blabbermouth.net.s3.amazonaws.com/media/amityafflictionheartbreak.jpg" height="20px" width="20px")
+              h5.contact__link: a(href="#"): img.image(src="http://assets.blabbermouth.net.s3.amazonaws.com/media/amityafflictionheartbreak.jpg" height="20px" width="20px")
 
       el-col(:span="10" style="box-sizing: content-box;")
         el-container.center-container(style="align-items:center")
@@ -37,9 +40,9 @@
         el-container(direction="vertical" style="align-items:center")
             h3.developer__name Naman Sehgal
             h5.developer__role UI
-            h5.contact__link: a(class="tel" href="#") Telegram 
-              a(class="git" href="#") GitHub
-
+            el-container(direction="horizontal")
+              h5.contact__link: a(href="#"): img.image(src="http://assets.blabbermouth.net.s3.amazonaws.com/media/amityafflictionheartbreak.jpg" height="20px" width="20px")
+              h5.contact__link: a(href="#"): img.image(src="http://assets.blabbermouth.net.s3.amazonaws.com/media/amityafflictionheartbreak.jpg" height="20px" width="20px")
 </template>
 <style scoped>
 .developer__name{
@@ -50,15 +53,8 @@
   margin-bottom: 0px;
 }
 .contact__link{
-  margin-top: 0px;
-}
-.tel{
-  text-decoration: none;
-  color: #0088cc;
-}
-.git{
-  text-decoration: none;
-  color: #6e5494;
+  margin-top: 4px;
+  padding: 2px
 }
   
 </style>
