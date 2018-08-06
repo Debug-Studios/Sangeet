e7efe2f0ce973fb6fe0afff6db0670fb31fe5356
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2018-08-06 08:23:22 +0530

    Added Nowplayingqueue component to git

---
+++ src/renderer/components/LandingPage/NowPlayingQueue.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  el-menu(default-active="2" @open="handleOpen" @close="handleClose" :collapse="isCollapse")
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isCollapse: true
+    };
+  },
+  methods: {
+    handleOpen (key,keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {}
+  }
+}
+</script>
+
